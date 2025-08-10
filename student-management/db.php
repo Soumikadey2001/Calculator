c502930aced315b2bb1a5783cb82cb1726612d2f
Soumika Dey <soumikadey61@gmail.com>
2025-08-10 21:52:38 +0530

    Update db.php

--- student-management/db.php
+++ student-management/db.php
@@ -1,19 +1,20 @@
 <?php
-// ডেভেলপমেন্টে error দেখা যাবে এমন করে রাখছি (প্রসোডাকশনে সরাবে)
+// Will show development error
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// mysqli দিয়ে কানেকশন তৈরি
-$servername = "localhost";    // XAMPP এ সবসময় localhost
-$username = "root";           // ডিফল্ট XAMPP ইউজার
-$password = "";               // ডিফল্ট XAMPP পাসওয়ার্ড খালি
-$dbname = "student_db";       // তুমি phpMyAdmin এ যেটা তৈরি করেছো
+// mysqli connection
+$servername = "localhost";    
+$username = "root";           
+$password = "";               
+$dbname = "student_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// যদি কানেকশন এরর হয় থেটা দেখাবে
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
